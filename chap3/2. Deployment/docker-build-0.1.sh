b2aa5c3039b803abfdd980f94b0b4acdb5cf02eb
Lux <fails9503@gmail.com>
2022-04-28 14:05:27 +0900

    Docker build 0.1, 0.2 version

---
+++ chap3/2. Deployment/docker-build-0.1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cat <<EOF > Dockerfile
+
+# Start with a base image containing Java runtime
+FROM openjdk:11
+
+# Add Author info
+LABEL maintainer="fails9503@gmail.com"
+
+# Add a volume to /tmp
+VOLUME /tmp
+
+# Make port 8080 available to the world outside this container
+EXPOSE 8080
+
+# The application's jar file
+ARG JAR_FILE=temp-0.0.1-SNAPSHOT.jar
+
+# Add the application's jar to the container
+ADD \${JAR_FILE} to-do-springboot.jar
+
+# Run the jar file
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/to-do-springboot.jar"]
+
+EOF
+
+docker build -t myweb:0.1 .
--- chap3/2. Deployment/docker-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t myweb:0.1 .
